4d2dac7d47a890866fbe1e7c93f2b170455d0008
Shubham Gupta <shubham180695.sg@gmail.com>
2020-07-05 01:53:52 -0700

    Add tests for leetcode problem 1467
    - All tests are based on examples given on leetcode website

---
+++ src/test/java/leetcode/_1467Test.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class _1467Test {
+
+    private static final double DELTA = 1e-5;
+
+    @Test
+    public void solve1() {
+        final _1467 solver = new _1467(new int[]{1, 1});
+        Assert.assertEquals(1.00000, solver.solve(), DELTA);
+    }
+
+    @Test
+    public void solve2() {
+        final _1467 solver = new _1467(new int[]{2, 1, 1});
+        Assert.assertEquals(0.66667, solver.solve(), DELTA);
+    }
+
+    @Test
+    public void solve3() {
+        final _1467 solver = new _1467(new int[]{1, 2, 1, 2});
+        Assert.assertEquals(0.60000, solver.solve(), DELTA);
+    }
+
+    @Test
+    public void solve4() {
+        final _1467 solver = new _1467(new int[]{3, 2, 1});
+        Assert.assertEquals(0.30000, solver.solve(), DELTA);
+    }
+
+    @Test
+    public void solve5() {
+        final _1467 solver = new _1467(new int[]{6, 6, 6, 6, 6, 6});
+        Assert.assertEquals(0.90327, solver.solve(), DELTA);
+    }
+}
