4c36df90a9f5071e82f038f179640cc3949fc881
Shubham Gupta <shubham180695.sg@gmail.com>
2020-08-01 18:05:43 -0700

    Add more test for _3385

--- src/test/java/leetcode/TreeNodeTest.java
+++ src/test/java/leetcode/TreeNodeTest.java
@@ -1,27 +1,43 @@
 package leetcode;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TreeNodeTest {
 
-    private TreeNode[] nodes = new TreeNode[8];
-
-    @Before
-    public void setup() {
-        nodes[7] = new TreeNode(9);
-        nodes[6] = null;
-        nodes[5] = new TreeNode(3);
-        nodes[4] = new TreeNode(5);
-        nodes[3] = new TreeNode(2, null, nodes[7]);
-        nodes[2] = new TreeNode(3, nodes[4], nodes[5]);
-        nodes[1] = new TreeNode(1, nodes[2], nodes[3]);
+    /**
+     * @param vals value at index 0 is ignored
+     * @return an array of nodes with root at index 1
+     */
+    private static TreeNode[] getNodeArray(final Integer[] vals) {
+        final TreeNode[] nodes = new TreeNode[vals.length];
+        for (int i = vals.length - 1; i > 0; i--) {
+            if (vals[i] == null) {
+                nodes[i] = null;
+                continue;
+            }
+            final int leftChildIndex = 2 * i;
+            if (leftChildIndex < vals.length) {
+                final TreeNode leftChild = nodes[leftChildIndex];
+                final int rightChildIndex = 2 * i + 1;
+                final TreeNode rightChild = rightChildIndex < vals.length ? nodes[rightChildIndex] : null;
+                nodes[i] = new TreeNode(vals[i], leftChild, rightChild);
+            } else {
+                nodes[i] = new TreeNode(vals[i]);
+            }
+        }
+        return nodes;
     }
 
     @Test
     public void test() {
+        assertWidth(4, new Integer[]{null, 1, 2, 3, 4, 5, null, 7});
+        assertWidth(2, new Integer[]{null, 1, 3, 2, 5});
+    }
+
+    private void assertWidth(final int expectedWidth, final Integer[] vals) {
+        final TreeNode[] nodes = getNodeArray(vals);
         final _3385 solver = new _3385();
-        Assert.assertEquals(4, solver.widthOfBinaryTree(nodes[1]));
+        Assert.assertEquals(expectedWidth, solver.widthOfBinaryTree(nodes[1]));
     }
 }
