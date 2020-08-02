bb587c8f3881f0cbb555b2a8907c31598eb9c329
Shubham Gupta <shubham180695.sg@gmail.com>
2020-08-01 20:47:23 -0700

    Move private method to the bottom

--- src/test/java/leetcode/_3386Test.java
+++ src/test/java/leetcode/_3386Test.java
@@ -7,6 +7,35 @@ import static org.junit.Assert.assertNull;
 
 public class _3386Test {
 
+    @Test
+    public void flatten() {
+        final Node input = getNodes(new Integer[]{1, 2, 3, 4, 5, 6, null, null, null, 7, 8, 9, 10, null, null, 11, 12});
+        final _3386 solver = new _3386();
+        Node node = solver.flatten(input);
+        final int[] expectedOrder = new int[]{1, 2, 3, 7, 8, 11, 12, 9, 10, 4, 5, 6};
+        for (int i = 0; i < expectedOrder.length; i++) {
+            System.out.println(node);
+            assert node != null;
+            assertNull(node.child);
+
+            assertEquals(expectedOrder[i], node.val);
+
+            if (i == 0) {
+                assertNull(node.prev);
+            } else {
+                assertEquals(expectedOrder[i - 1], node.prev.val);
+            }
+
+            if (i == expectedOrder.length - 1) {
+                assertNull(node.next);
+            } else {
+                assertEquals(expectedOrder[i + 1], node.next.val);
+            }
+
+            node = node.next;
+        }
+    }
+
     private static Node getNodes(final Integer[] vals) {
         Node prevLevel = null;
         Node prevNode = null;
@@ -41,34 +70,5 @@ public class _3386Test {
             }
         }
         return firstNode;
-    }
-
-    @Test
-    public void flatten() {
-        final Node input = getNodes(new Integer[]{1, 2, 3, 4, 5, 6, null, null, null, 7, 8, 9, 10, null, null, 11, 12});
-        final _3386 solver = new _3386();
-        Node node = solver.flatten(input);
-        final int[] expectedOrder = new int[]{1, 2, 3, 7, 8, 11, 12, 9, 10, 4, 5, 6};
-        for (int i = 0; i < expectedOrder.length; i++) {
-            System.out.println(node);
-            assert node != null;
-            assertNull(node.child);
-
-            assertEquals(expectedOrder[i], node.val);
-
-            if (i == 0) {
-                assertNull(node.prev);
-            } else {
-                assertEquals(expectedOrder[i - 1], node.prev.val);
-            }
-
-            if (i == expectedOrder.length - 1) {
-                assertNull(node.next);
-            } else {
-                assertEquals(expectedOrder[i + 1], node.next.val);
-            }
-
-            node = node.next;
-        }
     }
 }
