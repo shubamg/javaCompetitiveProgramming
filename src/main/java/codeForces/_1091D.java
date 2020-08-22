29715f01f6118d4c6dfd2ab2bde14ef45af2d12e
Shubham Gupta <shubham180695.sg@gmail.com>
2020-08-22 15:17:31 -0700

    Solve codeforces question 1091D

---
+++ src/main/java/codeForces/_1091D.java
@@ -0,0 +1,36 @@
+package codeForces;
+
+import io.InputReader;
+import math.ModuloCalculator;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _1091D {
+    private final static long MOD_BASE = 998244353L;
+    private final int N;
+    private final ModuloCalculator moduloCalculator = new ModuloCalculator(MOD_BASE);
+
+    _1091D(final int N) {
+        this.N = N;
+    }
+
+    public static void main(String[] args) {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final InputReader reader = new InputReader(br);
+        final int N = reader.nextInt();
+        final _1091D solver = new _1091D(N);
+        System.out.println(solver.solve());
+    }
+
+    long solve() {
+        long factorial = 1;
+        long badCuts = 0;
+        for (int i = 2; i <= N; i++) {
+            factorial = moduloCalculator.multiply(factorial, i);
+            badCuts = moduloCalculator.add(moduloCalculator.multiply(badCuts, i), moduloCalculator.power(i - 1, 2));
+        }
+        final long totalCuts = moduloCalculator.subtract(moduloCalculator.multiply(N, factorial), N - 1);
+        return totalCuts - badCuts;
+    }
+}
---
+++ src/test/java/codeForces/_1091DTest.java
@@ -0,0 +1,33 @@
+package codeForces;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class _1091DTest {
+    @Test
+    public void solveFor2() {
+        final _1091D solver = new _1091D(2);
+        MatcherAssert.assertThat(solver.solve(), is(equalTo(2L)));
+    }
+
+    @Test
+    public void solveFor3() {
+        final _1091D solver = new _1091D(3);
+        MatcherAssert.assertThat(solver.solve(), is(equalTo(9L)));
+    }
+
+    @Test
+    public void solveFor4() {
+        final _1091D solver = new _1091D(4);
+        MatcherAssert.assertThat(solver.solve(), is(equalTo(56L)));
+    }
+
+    @Test
+    public void solveFor10() {
+        final _1091D solver = new _1091D(10);
+        MatcherAssert.assertThat(solver.solve(), is(equalTo(30052700L)));
+    }
+}
