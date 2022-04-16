e4ee33cd23a6a1cb5dc7b5e2f997d4a00b7a3a62
Shubham Gupta <shubham180695.sg@gmail.com>
2022-04-15 21:43:53 -0700

    Add method getInitMatrix()

--- src/main/java/codeJam/_2021/InfiniTree.java
+++ src/main/java/codeJam/_2021/InfiniTree.java
@@ -1,5 +1,6 @@
 package codeJam._2021;
 
+import math.MatrixCalculator;
 import util.Pair;
 
 import java.util.List;
@@ -12,12 +13,27 @@ public class InfiniTree {
     final int N;
     final long A;
     final long B;
-    final List<Pair<Integer, Integer>> childNodes;
+    // 0 is leaf (White) and 1 is root
+    final List<Pair<Integer, Integer>> childNodePais;
 
-    public InfiniTree(final int n, final long a, final long b, final List<Pair<Integer, Integer>> childNodes) {
+    public InfiniTree(final int n, final long a, final long b, final List<Pair<Integer, Integer>> childNodePais) {
         N = n;
         A = a;
         B = b;
-        this.childNodes = childNodes;
+        this.childNodePais = childNodePais;
     }
+
+    long[][] getInitMatrix() {
+        final long[][] multiplierMatrix = MatrixCalculator.getZeroMatrix(N);
+        int parent = 0;
+        for (final Pair<Integer, Integer> childNodePair : childNodePais) {
+            final int leftNode = childNodePair.getKey() - 1;
+            final int rightNode = childNodePair.getValue() - 1;
+            multiplierMatrix[leftNode][parent] += 1;
+            multiplierMatrix[rightNode][parent] += 1;
+            parent++;
+        }
+        return multiplierMatrix;
+    }
+
 }
