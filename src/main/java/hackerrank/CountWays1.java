e9d903ef06ecb5a96060affb2ef8909c65a95f5e
shubamg <shubham180695.sg@gmail.com>
2022-06-03 03:40:17 -0700

    Add class CountWays1

---
+++ src/main/java/hackerrank/CountWays1.java
@@ -0,0 +1,25 @@
+package hackerrank;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Created by Shubham Gupta
+ * on 03 Jun 2022.
+ */
+public class CountWays1 {
+    private final int L;
+
+    private CountWays1(final int l) {
+        L = l;
+    }
+
+    public static int countWays(List<Integer> arr, long l, long r) {
+        // Write your code here
+
+    }
+
+    private int getL(final List<Integer> arr) {
+        return new HashSet<>(arr).stream().mapToInt(e -> e).reduce((a, b) -> a * b).getAsInt();
+    }
+}
