dba35757c68f8b7da2e206495bbc304dab9aaaba
shubamg <shubham180695.sg@gmail.com>
2022-06-03 03:46:07 -0700

    Add fields and constructor

--- src/main/java/hackerrank/CountWays1.java
+++ src/main/java/hackerrank/CountWays1.java
@@ -9,17 +9,23 @@ import java.util.List;
  */
 public class CountWays1 {
     private final int commonMultiple;
+    private final List<Integer> arr;
+    private final long l;
+    private final long r;
 
-    private CountWays1(final int commonMultiple) {
+    private CountWays1(final List<Integer> arr, final int commonMultiple, final long l, final long r) {
         this.commonMultiple = commonMultiple;
+        this.arr = arr;
+        this.l = l;
+        this.r = r;
     }
 
     public static int countWays(List<Integer> arr, long l, long r) {
-        // Write your code here
+        final CountWays1 solver = new CountWays1(arr, getCommonMultiple(arr), l, r);
 
     }
 
-    private int getCommonMultiple(final List<Integer> arr) {
+    private static int getCommonMultiple(final List<Integer> arr) {
         assert arr.size() > 0;
         return new HashSet<>(arr).stream().mapToInt(e -> e).reduce((a, b) -> a * b).getAsInt();
     }
