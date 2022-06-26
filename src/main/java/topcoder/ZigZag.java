56df826602ba2f59d80f28a84b7dea6f161932c8
shubamg <shubham180695.sg@gmail.com>
2022-06-25 21:23:14 -0700

    Use stream for better readability

--- src/main/java/topcoder/ZigZag.java
+++ src/main/java/topcoder/ZigZag.java
@@ -2,10 +2,14 @@ package topcoder;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.OptionalInt;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.function.IntPredicate;
 
 public class ZigZag {
+
+    private static final boolean DEBUG = false;
 
     public static void main(String[] args) {
         final ZigZag solver = new ZigZag();
@@ -19,41 +23,31 @@ public class ZigZag {
         final SortedMap<Integer, Integer> lastElem2NumDecSeq = new TreeMap<>();
         final SortedMap<Integer, Integer> lastElem2NumIncSeq = new TreeMap<>(Comparator.<Integer>naturalOrder()
                                                                                        .reversed());
-        for (final int x : sequence) {
-            final int decSeqEndingAtX = getDecSeqEndingAtX(lastElem2NumIncSeq, x);
-            final int incSeqEndingAtX = getIncSeqEndingAtX(lastElem2NumDecSeq, x);
-            lastElem2NumDecSeq.compute(x, (_k, oldV) -> oldV == null ? decSeqEndingAtX : decSeqEndingAtX + oldV);
-            lastElem2NumIncSeq.compute(x, (_k, oldV) -> oldV == null ? incSeqEndingAtX : incSeqEndingAtX + oldV);
-            System.out.printf("%d%ndec: %s%ninc: %s%n%n", x, lastElem2NumDecSeq, lastElem2NumIncSeq);
+        for (final int currElem : sequence) {
+            final int decSeqEndingAtX = getNumValidSeq(lastElem2NumIncSeq, lastElem -> lastElem > currElem);
+            final int incSeqEndingAtX = getNumValidSeq(lastElem2NumDecSeq, lastElem -> lastElem < currElem);
+            lastElem2NumDecSeq.compute(currElem, (_k, oldV) -> getMax(oldV, decSeqEndingAtX));
+            lastElem2NumIncSeq.compute(currElem, (_k, oldV) -> getMax(oldV, incSeqEndingAtX));
+            if (DEBUG) {
+                System.out.printf("%d%ndec: %s%ninc: %s%n%n", currElem, lastElem2NumDecSeq, lastElem2NumIncSeq);
+            }
         }
         final int longestDecSeq = lastElem2NumDecSeq.values().stream().mapToInt(e -> e).max().getAsInt();
         final int longestIncSeq = lastElem2NumIncSeq.values().stream().mapToInt(e -> e).max().getAsInt();
         return Math.max(longestDecSeq, longestIncSeq);
     }
 
-    private int getIncSeqEndingAtX(final SortedMap<Integer, Integer> lastElem2NumDecSeq, final int x) {
-        int incSeqEndingAtX = 1;
-        for (final Map.Entry<Integer, Integer> entry : lastElem2NumDecSeq.entrySet()) {
-            final int lastElement = entry.getKey();
-            if (lastElement < x) {
-                incSeqEndingAtX += entry.getValue();
-            } else {
-                break;
-            }
-        }
-        return incSeqEndingAtX;
+    private static int getMax(final Integer oldV, final int decSeqEndingAtX) {
+        return oldV == null ? decSeqEndingAtX : Math.max(decSeqEndingAtX, oldV);
     }
 
-    private int getDecSeqEndingAtX(final SortedMap<Integer, Integer> lastElem2NumIncSeq, final int x) {
-        int decSeqEndingAtX = 1;
-        for (final Map.Entry<Integer, Integer> entry : lastElem2NumIncSeq.entrySet()) {
-            final int lastElement = entry.getKey();
-            if (lastElement > x) {
-                decSeqEndingAtX += entry.getValue();
-            } else {
-                break;
-            }
-        }
-        return decSeqEndingAtX;
+    private static int getNumValidSeq(final SortedMap<Integer, Integer> lastElem2NumValidSeq,
+                                      final IntPredicate filter) {
+        final OptionalInt maxPrevLen = lastElem2NumValidSeq.entrySet()
+                                                           .stream()
+                                                           .filter(e -> filter.test(e.getKey()))
+                                                           .mapToInt(Map.Entry::getValue)
+                                                           .max();
+        return maxPrevLen.isPresent() ? maxPrevLen.getAsInt() + 1 : 1;
     }
 }
