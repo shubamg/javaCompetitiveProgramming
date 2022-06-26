329feb2c1eef1ee2807f0aaecff1208c0c022a4f
shubamg <shubham180695.sg@gmail.com>
2022-06-25 21:26:36 -0700

    Migrate maps from SortedMap -> Map

--- src/main/java/topcoder/ZigZag.java
+++ src/main/java/topcoder/ZigZag.java
@@ -3,7 +3,6 @@ package topcoder;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.OptionalInt;
-import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.function.IntPredicate;
 
@@ -20,32 +19,34 @@ public class ZigZag {
         if (sequence.length == 0) {
             return 0;
         }
-        final SortedMap<Integer, Integer> lastElem2NumDecSeq = new TreeMap<>();
-        final SortedMap<Integer, Integer> lastElem2NumIncSeq = new TreeMap<>(Comparator.<Integer>naturalOrder()
-                                                                                       .reversed());
+        final Map<Integer, Integer> lastElem2NumDecSeq = new TreeMap<>();
+        final Map<Integer, Integer> lastElem2NumIncSeq = new TreeMap<>(Comparator.<Integer>naturalOrder().reversed());
         for (final int currElem : sequence) {
             final int decSeqEndingAtX = getNumValidSeq(lastElem2NumIncSeq, lastElem -> lastElem > currElem);
             final int incSeqEndingAtX = getNumValidSeq(lastElem2NumDecSeq, lastElem -> lastElem < currElem);
-            lastElem2NumDecSeq.compute(currElem, (_k, oldV) -> getMax(oldV, decSeqEndingAtX));
-            lastElem2NumIncSeq.compute(currElem, (_k, oldV) -> getMax(oldV, incSeqEndingAtX));
+            lastElem2NumDecSeq.compute(currElem, (k, oldV) -> getMax(oldV, decSeqEndingAtX));
+            lastElem2NumIncSeq.compute(currElem, (k, oldV) -> getMax(oldV, incSeqEndingAtX));
             if (DEBUG) {
                 System.out.printf("%d%ndec: %s%ninc: %s%n%n", currElem, lastElem2NumDecSeq, lastElem2NumIncSeq);
             }
         }
-        final int longestDecSeq = lastElem2NumDecSeq.values().stream().mapToInt(e -> e).max().getAsInt();
-        final int longestIncSeq = lastElem2NumIncSeq.values().stream().mapToInt(e -> e).max().getAsInt();
+        final int longestDecSeq = getMaxVal(lastElem2NumDecSeq);
+        final int longestIncSeq = getMaxVal(lastElem2NumIncSeq);
         return Math.max(longestDecSeq, longestIncSeq);
+    }
+
+    private static int getMaxVal(final Map<Integer, Integer> lastElem2NumDecSeq) {
+        return lastElem2NumDecSeq.values().stream().mapToInt(e -> e).max().orElse(0);
     }
 
     private static int getMax(final Integer oldV, final int decSeqEndingAtX) {
         return oldV == null ? decSeqEndingAtX : Math.max(decSeqEndingAtX, oldV);
     }
 
-    private static int getNumValidSeq(final SortedMap<Integer, Integer> lastElem2NumValidSeq,
-                                      final IntPredicate filter) {
+    private static int getNumValidSeq(final Map<Integer, Integer> lastElem2NumValidSeq, final IntPredicate keyFilter) {
         final OptionalInt maxPrevLen = lastElem2NumValidSeq.entrySet()
                                                            .stream()
-                                                           .filter(e -> filter.test(e.getKey()))
+                                                           .filter(e -> keyFilter.test(e.getKey()))
                                                            .mapToInt(Map.Entry::getValue)
                                                            .max();
         return maxPrevLen.isPresent() ? maxPrevLen.getAsInt() + 1 : 1;
