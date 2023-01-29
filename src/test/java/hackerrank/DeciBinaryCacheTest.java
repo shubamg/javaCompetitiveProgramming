e4e37c352d7c086d12947170666148abd54ad5ae
shubamg <shubham180695.sg@gmail.com>
2023-01-29 13:03:20 -0800

    Add ut compareDifferentKeys()

--- src/test/java/hackerrank/DeciBinaryCacheTest.java
+++ src/test/java/hackerrank/DeciBinaryCacheTest.java
@@ -92,6 +92,14 @@ public class DeciBinaryCacheTest {
         Assert.assertTrue(largerKey.compareTo(smallerKey) > 0);
     }
 
+    @Test
+    public void compareDifferentKeys() {
+        final DecimalToDeciBinary.Key smallerKey = new DecimalToDeciBinary.Key(4, 2);
+        final DecimalToDeciBinary.Key largerKey = new DecimalToDeciBinary.Key(5, 1);
+        Assert.assertTrue(smallerKey.compareTo(largerKey) < 0);
+        Assert.assertTrue(largerKey.compareTo(smallerKey) > 0);
+    }
+
     private static int[] copyArray(
             final int[] numDigitsToMaxDecimal, @SuppressWarnings("SameParameterValue") final int start,
             final int rangeLength) {
